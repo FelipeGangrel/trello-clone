c8467df0b233563d7506af434269e73df8d5de29
Felipe Gangrel <felipefrog@gmail.com>
2023-12-20 17:27:19 -0300

    Add prettier.config.js file

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['tv', 'twMerge', 'clsx', 'cn'],
+  tailwindConfig: './tailwind.config.ts',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+}
