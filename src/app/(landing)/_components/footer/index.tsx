7cc050713f45729d57327ebec803ea56e8848b74
Felipe Gangrel <felipefrog@gmail.com>
2023-12-20 20:39:37 -0300

    Add footer component

---
+++ src/app/(landing)/_components/footer/index.tsx
@@ -0,0 +1,22 @@
+import { Logo } from '@/components/logo'
+import { Button } from '@/components/ui'
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full border-t bg-slate-100">
+      <div className="container flex items-center justify-between py-4">
+        <Logo />
+        <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of service
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { Footer }
