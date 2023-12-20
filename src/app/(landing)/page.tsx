08e650e5e698a8a5249126e097b3f02751e0bc81
Felipe Gangrel <felipefrog@gmail.com>
2023-12-20 20:40:06 -0300

    Add font and button styles to landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,26 @@
 import { Medal } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui'
+import { headingFont } from '@/fonts'
+import { frontend } from '@/lib/routes'
+import { cn } from '@/lib/utils'
+
+const textFont = Poppins({
+  subsets: ['latin'],
+  weight: ['300'],
+})
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
+      <div
+        className={cn(
+          'flex flex-col items-center justify-center',
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center rounded-full border bg-amber-100 p-4 uppercase text-amber-700 shadow-sm">
           <Medal size={24} className="mr-2" />
           <span>The best task manager</span>
@@ -15,11 +32,19 @@ const LandingPage = () => {
           work forward.
         </div>
       </div>
-      <div className="mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl">
+      <div
+        className={cn(
+          'mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl',
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
       </div>
+      <Button asChild className="mt-6">
+        <Link href={frontend.signUp()}>Get Taskify for free</Link>
+      </Button>
     </div>
   )
 }
