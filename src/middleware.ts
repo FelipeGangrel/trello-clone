cf0139d18f6fc70a39e045446540a0c928df034d
Felipe Gangrel <felipefrog@gmail.com>
2023-12-21 18:33:29 -0300

    Add afterAuth logic to redirect users to the appropriate organization selection page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,26 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+import { frontend } from '@/lib/routes'
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ['/'],
+  afterAuth: (auth, req) => {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = frontend.selectOrganization()
+
+      if (auth.orgId) {
+        path = frontend.organization(auth.orgId)
+      }
+
+      const orgSelection = new URL(path, req.url)
+
+      return NextResponse.redirect(orgSelection)
+    }
+  },
 })
 
 export const config = {
