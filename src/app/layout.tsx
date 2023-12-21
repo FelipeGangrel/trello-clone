304e1758270fb5b684f33cc935749723f3c30d75
Felipe Gangrel <felipefrog@gmail.com>
2023-12-21 19:04:12 -0300

    Add ThemeProvider to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { ThemeProvider } from '@/components/providers'
 import { siteConfig } from '@/config/site'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -28,7 +29,16 @@ type LayoutProps = {
 const RootLayout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
