5074f0829fa90f0428e19aff85fba577f52fd4e4
Felipe Gangrel <felipefrog@gmail.com>
2023-12-21 16:46:53 -0300

    Add routes and common types

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,6 +1,10 @@
+import type { Id } from '@/types/common'
+
 const frontend = {
   signIn: () => process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
   signUp: () => process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+  selectOrganization: () => '/select-org',
+  organization: (id: Id) => `/organization/${id}`,
 }
 
 export { frontend }
