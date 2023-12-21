b6e8b0d2889418b1f01456607f884797956bf0a3
Felipe Gangrel <felipefrog@gmail.com>
2023-12-21 18:23:50 -0300

    Update navbar background color

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -6,7 +6,7 @@ import { frontend } from '@/lib/routes'
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full border-b bg-white shadow-sm">
+    <div className="fixed top-0 w-full border-b bg-background shadow-sm">
       <div className="container flex h-14 items-center justify-between">
         <Logo />
         <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
