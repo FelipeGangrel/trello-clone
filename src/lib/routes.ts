51472c3652bfff18a99ff1ad63c62a0f4434a345
Felipe Gangrel <felipefrog@gmail.com>
2023-12-21 18:23:33 -0300

    Update organization ID parameter type in routes.ts

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,10 +1,8 @@
-import type { Id } from '@/types/common'
-
 const frontend = {
   signIn: () => process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
   signUp: () => process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
   selectOrganization: () => '/select-org',
-  organization: (id: Id) => `/organization/${id}`,
+  organization: (id: string) => `/organization/${id}`,
 }
 
 export { frontend }
