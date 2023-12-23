fe8d24aec8e857397837b2ecc893bbdec5ff9ffd
Felipe Gangrel <felipefrog@gmail.com>
2023-12-23 20:55:51 -0300

    Add serializeImage and parseImageString functions to unsplash.ts

--- src/lib/unsplash.ts
+++ src/lib/unsplash.ts
@@ -1,6 +1,30 @@
 import { createApi } from 'unsplash-js'
 
+import { UnsplashImage, UnsplashImageJSON } from '@/types/common'
+
 export const unsplash = createApi({
   accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY,
   fetch: fetch,
 })
+
+export function serializeImage(image: UnsplashImage): string {
+  const { id, urls, links, user } = image
+
+  return JSON.stringify({
+    id,
+    urls: {
+      thumb: urls.thumb,
+      full: urls.full,
+    },
+    links: {
+      html: links.html,
+    },
+    user: {
+      name: user.name,
+    },
+  } satisfies UnsplashImageJSON)
+}
+
+export function parseImageString(value: string): UnsplashImageJSON {
+  return JSON.parse(value) as UnsplashImageJSON
+}
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,19 @@
+import { FALLBACK_UNSPLASH_IMAGES } from '@/constants/images'
+
 export type Id = string | number
+
+export type UnsplashImage = (typeof FALLBACK_UNSPLASH_IMAGES)[0]
+
+export type UnsplashImageJSON = {
+  id: string
+  urls: {
+    thumb: string
+    full: string
+  }
+  links: {
+    html: string
+  }
+  user: {
+    name: string
+  }
+}
