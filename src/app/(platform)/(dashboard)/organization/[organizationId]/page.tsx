bb1ce463a29554bf55a58e0aac8c9749b1bab433
Felipe Gangrel <felipefrog@gmail.com>
2023-12-23 17:32:41 -0300

    Add Separator and BoardList components to OrganizationPage

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,15 @@
-import { Info } from './_components'
+import { Separator } from '@/components/ui'
+
+import { BoardList, Info } from './_components'
 
 const OrganizationPage = async () => {
   return (
     <div className="mb-20 w-full">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   )
 }
