94666158a76f19f0d3b52ddfa3f7526b1082edd3
Felipe Gangrel <felipefrog@gmail.com>
2023-12-23 15:17:59 -0300

    Update logo component

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -8,8 +8,14 @@ export const Logo = () => {
   return (
     <div className="logo">
       <Link href="/" className="focus-ring inline-block rounded-sm">
-        <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
-          <Image src="/images/logo.svg" width={30} height={30} alt="logo" />
+        <div className="relative hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
+          <Image
+            src="/images/logo.svg"
+            width={0}
+            height={0}
+            className="h-auto w-[30px]"
+            alt="logo"
+          />
           <p
             className={cn(
               'pb-1 text-lg text-neutral-700',
