6208919229ca6f839c49df3c48c25dbac51bd541
Felipe Gangrel <felipefrog@gmail.com>
2023-12-23 18:07:34 -0300

    Add CreateBoardPopover component to BoardList

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list/index.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list/index.tsx
@@ -2,6 +2,7 @@
 
 import { HelpCircleIcon, User2Icon } from 'lucide-react'
 
+import { CreateBoardPopover } from '@/components/create-board-popover'
 import { Hint } from '@/components/hint'
 
 export const BoardList = async () => {
@@ -12,20 +13,22 @@ export const BoardList = async () => {
         <span>Your boards</span>
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-        <div
-          role="button"
-          className="relative flex aspect-video h-full w-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
-        >
-          <p className="text-sm">Create new board</p>
-          <span className="text-xs">5 remaining</span>
-          <Hint
-            sideOffset={40}
-            side="bottom"
-            description="Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace"
+        <CreateBoardPopover side="right" sideOffset={10}>
+          <div
+            role="button"
+            className="relative flex aspect-video h-full w-full flex-col items-center justify-center gap-y-1 rounded-sm bg-muted transition hover:opacity-75"
           >
-            <HelpCircleIcon className="absolute bottom-2 right-2 h-3 w-3" />
-          </Hint>
-        </div>
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 remaining</span>
+            <Hint
+              sideOffset={40}
+              side="bottom"
+              description="Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace"
+            >
+              <HelpCircleIcon className="absolute bottom-2 right-2 h-3 w-3" />
+            </Hint>
+          </div>
+        </CreateBoardPopover>
       </div>
     </div>
   )
