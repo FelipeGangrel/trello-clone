45c24227f9f4a78bf421fa9db0b91dbafb400304
Felipe Gangrel <felipefrog@gmail.com>
2023-12-23 11:14:52 -0300

    Add input component to UI

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './accordion'
 export * from './button'
 export * from './dropdown-menu'
+export * from './input'
 export * from './separator'
 export * from './sheet'
 export * from './skeleton'
