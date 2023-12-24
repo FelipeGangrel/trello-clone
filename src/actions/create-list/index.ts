c38d23ccfcc0b2c8440eb689736572c7bc738b2e
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 12:17:35 -0300

    Add create-list action and schema

---
+++ src/actions/create-list/index.ts
@@ -0,0 +1,73 @@
+'use server'
+
+import { auth } from '@clerk/nextjs'
+import { revalidatePath } from 'next/cache'
+
+import { createSafeAction } from '@/lib/create-safe-action'
+import { db } from '@/lib/db'
+import { frontend } from '@/lib/routes'
+
+import { CreateList } from './schema'
+import { InputType, ReturnType } from './types'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth()
+
+  if (!userId || !orgId) {
+    return {
+      errorMessage: 'Unauthorized',
+    }
+  }
+
+  const { boardId, title } = data
+
+  let list
+
+  try {
+    const board = await db.board.findUnique({
+      where: {
+        id: boardId,
+        orgId,
+      },
+    })
+
+    if (!board) {
+      return {
+        errorMessage: 'Board not found',
+      }
+    }
+
+    const lastList = await db.list.findFirst({
+      where: {
+        boardId,
+      },
+      orderBy: {
+        order: 'desc',
+      },
+      select: {
+        order: true,
+      },
+    })
+
+    const newOrder = lastList ? lastList.order + 1 : 1
+
+    list = await db.list.create({
+      data: {
+        boardId,
+        title,
+        order: newOrder,
+      },
+    })
+  } catch (error) {
+    return {
+      errorMessage: 'Failed to create list',
+    }
+  }
+
+  revalidatePath(frontend.board(boardId))
+  return {
+    data: list,
+  }
+}
+
+export const createList = createSafeAction(CreateList, handler)
