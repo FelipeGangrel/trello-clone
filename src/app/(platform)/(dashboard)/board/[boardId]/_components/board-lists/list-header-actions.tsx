f4a078025eecdec45e2efaa6e5c60f344cf9c6a6
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 15:37:41 -0300

    Refactor list header actions component

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/board-lists/list-header-actions.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/board-lists/list-header-actions.tsx
@@ -2,9 +2,14 @@
 
 import { List } from '@prisma/client'
 import { MoreHorizontalIcon } from 'lucide-react'
+import type { ElementRef } from 'react'
+import { useRef } from 'react'
+import { toast } from 'sonner'
 
-import { SubmitButton } from '@/components/form'
+import { deleteList } from '@/actions/delete-list'
+import { duplicateList } from '@/actions/duplicate-list'
 import { Button, Popover, Separator } from '@/components/ui'
+import { useAction } from '@/hooks'
 
 type ListHeaderActionsProps = {
   list: List
@@ -15,6 +20,42 @@ export const ListHeaderActions = ({
   list,
   onAddCard,
 }: ListHeaderActionsProps) => {
+  const closeButtonRef = useRef<ElementRef<'button'>>(null)
+
+  const { execute: executeDelete, isLoading: isDeleting } = useAction(
+    deleteList,
+    {
+      onSuccess: (list) => {
+        toast.success(`List "${list.title}" was deleted`)
+        closeButtonRef.current?.click()
+      },
+      onError: (error) => {
+        toast.error(error)
+      },
+    }
+  )
+
+  const { execute: executeDuplicate, isLoading: isDuplicating } = useAction(
+    duplicateList,
+    {
+      onSuccess: (list) => {
+        toast.success(`List "${list.title}" was created`)
+        closeButtonRef.current?.click()
+      },
+      onError: (error) => {
+        toast.error(error)
+      },
+    }
+  )
+
+  const onDuplicate = () => {
+    executeDuplicate({ id: list.id, boardId: list.boardId })
+  }
+
+  const onDelete = () => {
+    executeDelete({ id: list.id, boardId: list.boardId })
+  }
+
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
@@ -31,31 +72,21 @@ export const ListHeaderActions = ({
         <div className="pb-4 text-center text-sm font-medium text-neutral-600">
           <p>List Actions</p>
         </div>
-        <Popover.CloseButton />
+        <Popover.CloseButton ref={closeButtonRef} />
         <Button variant="menu-action" onClick={onAddCard}>
           Add card...
         </Button>
-        <form>
-          <input type="hidden" name="id" id="id" value={list.id} />
-          <input
-            type="hidden"
-            name="boardId"
-            id="boardId"
-            value={list.boardId}
-          />
-          <SubmitButton variant="menu-action">Copy list...</SubmitButton>
-        </form>
+        <Button
+          variant="menu-action"
+          onClick={onDuplicate}
+          disabled={isDuplicating}
+        >
+          Duplicate this list...
+        </Button>
         <Separator className="my-4" />
-        <form>
-          <input type="hidden" name="id" id="id" value={list.id} />
-          <input
-            type="hidden"
-            name="boardId"
-            id="boardId"
-            value={list.boardId}
-          />
-          <SubmitButton variant="menu-action">Delete this list...</SubmitButton>
-        </form>
+        <Button variant="menu-action" onClick={onDelete} disabled={isDeleting}>
+          Delete this list
+        </Button>
       </Popover.Content>
     </Popover.Root>
   )
