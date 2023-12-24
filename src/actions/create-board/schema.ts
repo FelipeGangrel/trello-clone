3809bf01b31ac0103c3409022c9b9671981452f0
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 13:45:22 -0300

    Trim whitespace in title fields

--- src/actions/create-board/schema.ts
+++ src/actions/create-board/schema.ts
@@ -6,7 +6,8 @@ export const CreateBoard = z.object({
       required_error: 'Title is required',
       invalid_type_error: 'Title is required',
     })
-    .min(3, 'Title must be at least 3 characters long'),
+    .min(3, 'Title must be at least 3 characters long')
+    .trim(),
   image: z.string({
     required_error: 'Select an image for your board',
     invalid_type_error: 'Select an image for your board',
--- src/actions/create-list/schema.ts
+++ src/actions/create-list/schema.ts
@@ -7,5 +7,6 @@ export const CreateList = z.object({
       required_error: 'Title is required',
       invalid_type_error: 'Title is required',
     })
-    .min(3, 'Title must be at least 3 characters long'),
+    .min(3, 'Title must be at least 3 characters long')
+    .trim(),
 })
