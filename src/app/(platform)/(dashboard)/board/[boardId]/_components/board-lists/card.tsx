a7124533ad77f2a1e6a812c56b1ccd2f3a914c72
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 20:08:56 -0300

    Refactor card component styling

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/board-lists/card.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/board-lists/card.tsx
@@ -1,6 +1,8 @@
 import { Draggable } from '@hello-pangea/dnd'
 import type { Card as CardModel } from '@prisma/client'
 
+import { cn } from '@/lib/utils'
+
 type ListCardProps = {
   index: number
   card: CardModel
@@ -9,13 +11,16 @@ type ListCardProps = {
 export const Card = ({ index, card }: ListCardProps) => {
   return (
     <Draggable draggableId={card.id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
-          className="truncate rounded-md border-2 border-transparent bg-card px-3 py-2 text-sm shadow-sm hover:border-black"
+          className={cn(
+            'truncate rounded-md border-2 border-transparent bg-card px-3 py-2 text-sm shadow-sm transition hover:border-black',
+            snapshot.isDragging && 'shadow-lg'
+          )}
         >
           {card.title}
         </div>
