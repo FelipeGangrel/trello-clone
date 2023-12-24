8b4af763c78ebf753f5956c5b4653e4e789a588a
Felipe Gangrel <felipefrog@gmail.com>
2023-12-23 22:35:29 -0300

    Add generateMetadata function to layout.tsx

--- src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,7 +1,18 @@
+import { auth } from '@clerk/nextjs'
+import { startCase } from 'lodash'
+
 import { OrganizationWatcher } from './_components'
 
 type LayoutProps = {
   children: React.ReactNode
+}
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+
+  return {
+    title: startCase(orgSlug || 'organization'),
+  }
 }
 
 const OrganizationIdLayout = ({ children }: LayoutProps) => {
