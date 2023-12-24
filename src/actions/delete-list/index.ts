6953161ef5d5e820fe3600bc9314a67c723b0141
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 15:09:23 -0300

    Add deleteList action

---
+++ src/actions/delete-list/index.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import { auth } from '@clerk/nextjs'
+import { revalidatePath } from 'next/cache'
+
+import { createSafeAction } from '@/lib/create-safe-action'
+import { db } from '@/lib/db'
+import { frontend } from '@/lib/routes'
+
+import { DeleteList } from './schema'
+import { InputType, ReturnType } from './types'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth()
+
+  if (!userId || !orgId) {
+    return {
+      errorMessage: 'Unauthorized',
+    }
+  }
+
+  const { id, boardId } = data
+
+  let list
+
+  try {
+    list = await db.list.delete({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+    })
+  } catch (error) {
+    return {
+      errorMessage: 'Failed to delete list',
+    }
+  }
+
+  revalidatePath(frontend.board(boardId))
+  return {
+    data: list,
+  }
+}
+
+export const deleteList = createSafeAction(DeleteList, handler)
