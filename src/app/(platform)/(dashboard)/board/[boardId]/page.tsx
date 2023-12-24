7f15120bd30ebc9b896d707c01250bdc88585fc7
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 12:07:58 -0300

    Refactor BoardPage component to include authentication and fetch lists

--- src/app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,46 @@
-const BoardPage = async () => {
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+import { db } from '@/lib/db'
+import { frontend } from '@/lib/routes'
+
+import { ListContainer } from './_components'
+
+type BoardPageProps = {
+  params: {
+    boardId: string
+  }
+}
+
+const BoardPage = async ({ params }: BoardPageProps) => {
+  const { orgId } = auth()
+
+  if (!orgId) {
+    redirect(frontend.selectOrganization())
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  })
+
   return (
-    <div>
-      <h1>Board Page</h1>
+    <div className="h-full overflow-x-auto p-4">
+      <ListContainer boardId={params.boardId} lists={lists} />
     </div>
   )
 }
