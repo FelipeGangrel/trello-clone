5100514ba35e10dfafbb96ce42f110e65db4e641
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 14:43:53 -0300

    Refactor organization layout to include a sticky sidebar

--- src/app/(platform)/(dashboard)/organization/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/layout.tsx
@@ -8,7 +8,10 @@ const OrganizationLayout = ({ children }: LayoutProps) => {
   return (
     <main className="container pt-20 md:pt-24">
       <div className="flex gap-x-7">
-        <div className="hidden w-64 shrink-0 md:flex">
+        <div
+          id="sidebar-wrapper"
+          className="sticky top-[96px] hidden w-64 shrink-0 self-start md:flex"
+        >
           <Sidebar />
         </div>
         {children}
