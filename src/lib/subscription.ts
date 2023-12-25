d09430ba8a882f94505bf826bbc2f6a4b3383ce6
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 16:31:13 -0300

    Add Stripe integration and subscription check

--- src/lib/subscription.ts
+++ src/lib/subscription.ts
@@ -1,3 +1,36 @@
+import { auth } from '@clerk/nextjs'
+
+import { db } from './db'
+
+const DAYS_IN_MS = 1000 * 60 * 60 * 24
+
 export async function checkSubscription() {
-  return false
+  const { orgId } = auth()
+
+  if (!orgId) {
+    return false
+  }
+
+  const orgSubscription = await db.orgSubscription.findUnique({
+    where: { orgId },
+    select: {
+      stripeSubscriptionId: true,
+      stripeCurrentPeriodEnd: true,
+      stripeCustomerId: true,
+      stripePriceId: true,
+    },
+  })
+
+  if (!orgSubscription) {
+    return false
+  }
+
+  const { stripeCustomerId, stripeCurrentPeriodEnd } = orgSubscription
+
+  const isValid =
+    stripeCustomerId &&
+    // we even give the user a 1 day grace period
+    stripeCurrentPeriodEnd?.getTime()! + DAYS_IN_MS > Date.now()
+
+  return !!isValid
 }
