dc76d6f41fd88bbe80012b50611ac96facdd5a97
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 17:17:46 -0300

    Add NEXT_PUBLIC_APP_URL to .env.example and environment types

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -1,5 +1,6 @@
 namespace NodeJS {
   interface ProcessEnv {
+    NEXT_PUBLIC_APP_URL: string
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: string
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: string
     NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string
