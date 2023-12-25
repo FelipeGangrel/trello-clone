bff94574f69a89c3b6e322a7895339edd3b8c131
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 09:58:57 -0300

    Update card component styles

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/board-lists/card.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/board-lists/card.tsx
@@ -22,8 +22,8 @@ export const Card = ({ index, card }: ListCardProps) => {
           role="button"
           onClick={() => openModal(card.id)}
           className={cn(
-            'truncate rounded-md border-2 border-transparent bg-card px-3 py-2 text-sm shadow-sm transition hover:border-black',
-            snapshot.isDragging && 'shadow-lg'
+            'truncate rounded-md border-2 border-transparent bg-card px-3 py-2 text-sm shadow-sm transition-colors hover:border-black',
+            snapshot.isDragging && 'shadow-md'
           )}
         >
           {card.title}
