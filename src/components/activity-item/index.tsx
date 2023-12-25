27d7e9717a0d79138c610043550fd195b2774fdd
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 13:21:32 -0300

    Add ActivityItem component

---
+++ src/components/activity-item/index.tsx
@@ -0,0 +1,30 @@
+import { AuditLog } from '@prisma/client'
+import { format } from 'date-fns'
+
+import { Avatar } from '@/components/ui'
+import { generateLogMessage } from '@/lib/audit-log'
+
+interface ActivityItemProps {
+  log: AuditLog
+}
+
+export const ActivityItem = ({ log }: ActivityItemProps) => {
+  return (
+    <li className="flex items-center gap-x-2">
+      <Avatar.Root className="h-8 w-8">
+        <Avatar.Image src={log.userImage} />
+      </Avatar.Root>
+      <div className="flex flex-col space-y-0.5">
+        <p className="text-sm text-muted-foreground">
+          <span className="font-semibold lowercase text-neutral-700">
+            {log.userName}
+          </span>{' '}
+          {generateLogMessage(log)}
+        </p>
+        <p className="text-xs text-muted-foreground">
+          {format(new Date(log.createdAt), "MMM d, yyyy 'at' h:mm a")}
+        </p>
+      </div>
+    </li>
+  )
+}
