e270c6c77a6c4bf3b3263c4ef8c3d3a4e3e04241
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 00:08:11 -0300

    Remove unused import and add description component

--- src/components/modals/card-modal/index.tsx
+++ src/components/modals/card-modal/index.tsx
@@ -2,12 +2,13 @@
 
 import { useQuery } from '@tanstack/react-query'
 
-import { Dialog, Skeleton } from '@/components/ui'
+import { Dialog } from '@/components/ui'
 import { useCardModal } from '@/hooks/use-card-modal'
 import { fetcher } from '@/lib/fetcher'
 import { api } from '@/lib/routes'
 import { CardWithList } from '@/types/db'
 
+import { Description } from './description'
 import { Header } from './header'
 
 export const CardModal = () => {
@@ -24,6 +25,13 @@ export const CardModal = () => {
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
       <Dialog.Content>
         {card ? <Header card={card} /> : <Header.Skeleton />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {card ? <Description card={card} /> : <Description.Skeleton />}
+            </div>
+          </div>
+        </div>
       </Dialog.Content>
     </Dialog.Root>
   )
