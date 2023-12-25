96d0a1df4688b9b59bc987842ecad3a956bceb77
Felipe Gangrel <felipefrog@gmail.com>
2023-12-24 23:44:08 -0300

    Refactor card modal component to fetch and display card data

--- src/components/modals/card-modal/index.tsx
+++ src/components/modals/card-modal/index.tsx
@@ -1,16 +1,30 @@
 'use client'
 
-import { Dialog } from '@/components/ui'
+import { useQuery } from '@tanstack/react-query'
+
+import { Dialog, Skeleton } from '@/components/ui'
 import { useCardModal } from '@/hooks/use-card-modal'
+import { fetcher } from '@/lib/fetcher'
+import { api } from '@/lib/routes'
+import { CardWithList } from '@/types/db'
+
+import { Header } from './header'
 
 export const CardModal = () => {
-  const cardId = useCardModal((state) => state?.id)
+  const cardId = useCardModal((state) => state.id!)
   const isOpen = useCardModal((state) => state.isOpen)
   const onClose = useCardModal((state) => state.onClose)
 
+  const { data: card } = useQuery<CardWithList>({
+    queryKey: ['card', cardId],
+    queryFn: () => fetcher(api.fetchCard(cardId)),
+  })
+
   return (
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
-      <Dialog.Content>I am a modal</Dialog.Content>
+      <Dialog.Content>
+        {card ? <Header card={card} /> : <Header.Skeleton />}
+      </Dialog.Content>
     </Dialog.Root>
   )
 }
