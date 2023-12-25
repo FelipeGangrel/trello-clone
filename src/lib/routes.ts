84a33c52fd83bd9df661a0011f53013951c572e7
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 17:44:49 -0300

    Add baseUrl and prependAppUrl function to routes.ts

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,5 @@
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL
+
 export const frontend = {
   signIn: () => process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
   signUp: () => process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
@@ -7,6 +9,10 @@ export const frontend = {
   organizationSettings: (id: string) => `/organization/${id}/settings`,
   organizationBilling: (id: string) => `/organization/${id}/billing`,
   board: (id: string) => `/board/${id}`,
+}
+
+export const prependAppUrl = (endpoint: string) => {
+  return `${baseUrl}${endpoint}`
 }
 
 export const api = {
