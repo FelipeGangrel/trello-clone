5b37e5b5ef0f7f68f26fff4719e119ba8fe714cd
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 09:58:41 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 module.exports = {
   darkMode: ['class'],
   content: [
@@ -73,4 +74,4 @@ module.exports = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-}
+} satisfies Config
