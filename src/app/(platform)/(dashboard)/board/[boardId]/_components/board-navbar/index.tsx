cf90e4f816d272e97c6b2f11c977652d2bd9efd6
Felipe Gangrel <felipefrog@gmail.com>
2023-12-25 21:11:28 -0300

    Add back button to board navbar

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar/index.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar/index.tsx
@@ -1,4 +1,8 @@
 import type { Board } from '@prisma/client'
+import { ArrowLeftIcon } from 'lucide-react'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui'
 
 import { BoardActions } from './board-actions'
 import { BoardTitleForm } from './board-title-form'
@@ -11,6 +15,11 @@ export const BoardNavbar = async ({ board }: BoardNavbarProps) => {
   return (
     <div className="fixed top-14 z-[40] w-full items-center bg-black/50 text-white backdrop-blur">
       <div className="container flex h-14 items-center gap-x-4">
+        <Button variant="transparent" size="icon">
+          <Link href="/">
+            <ArrowLeftIcon />
+          </Link>
+        </Button>
         <BoardTitleForm board={board} />
         <div className="ml-auto">
           <BoardActions boardId={board.id} />
